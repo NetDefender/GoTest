b7f3f1a18c7777e8288481ad1cca224ca635e1ba
NetDefender <danielsith@hotmail.com>
2022-08-22 00:32:58 +0200

    First commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+	In console write:
+	go env GO111MODULE=off
+
+	It is when you write code outside of GO path
+*/
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
