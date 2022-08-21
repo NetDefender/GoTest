9501111a569b8eef4138b6f5a4f61160aad31c76
NetDefender <danielsith@hotmail.com>
2022-08-22 00:54:49 +0200

    Maps

--- main.go
+++ main.go
@@ -9,6 +9,16 @@ package main
 
 import "fmt"
 
+func Worker(values map[string]float64) float64 {
+	v := values["hola"]
+	fmt.Printf("%v", v)
+	return v
+}
+
 func main() {
+	v := map[string]float64{
+		"hola": 0.1,
+	}
+	Worker(v)
 	fmt.Println("Hello World")
 }
